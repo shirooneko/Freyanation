7a60a86fdf1c14d8666a9c561c8bf0e33ae98de6
Muhammad Apriyansyah <57085445+shirooneko@users.noreply.github.com>
2023-06-03 09:16:11 +0700

    jjj

--- app.js
+++ app.js
@@ -22,13 +22,6 @@ hbs.registerHelper('inc', function (value, options) {
     return parseInt(value) + 1;
 })
 
-const T = new Twit({
-    consumer_key: 'RjBFTDBncWdlTUt1cmwtRnk1VHk6MTpjaQ',
-    consumer_secret: 'r2gQs-JGr0rXVvGnit0phSjkmdfygQAprO0C-oGtwZ4IQg51r3',
-    access_token: '1213416161708085250-8XA2r4u4yUu4IaazUQLfztPJX6F4oo',
-    access_token_secret: 'lc87IkXTpbcx8NlvKFWaXLWsq5a3z1GWtQXrbnbP0pdZB',
-});
-
 // Route untuk halaman utama
 app.get('/', (req, res) => {
     res.render('homepage');
